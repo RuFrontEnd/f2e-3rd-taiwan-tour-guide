f3116ec1a6b0f6f05dd552c7898dd334226d4f40
RuFrontEnd <k718198@gmail.com>
2022-12-26 22:09:08 +0800

    切版 Card info 區塊

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -9,30 +9,37 @@ import { ReactComponent as Clock } from "assets/icon/clock.svg";
 const Card = (props: Types.Props) => {
   return (
     <CardRef>
+      <div className="fs-1 w-10 opacity-40">123</div>
       {props?.src && <CardRef.Img variant="top" src={props?.src} />}
       <CardRef.Body>
-        <CardRef.Title className="fw-bold" title={props?.title}>
+        <CardRef.Title className="fw-bold mb-2 fs-5" title={props?.title}>
           {props?.title}
         </CardRef.Title>
         {/* TODO: 地址 / 聯絡方式 / 開放時段 待製作 */}
-        {/* <div className="fw-normal mb-3">
+        <div className="fw-normal mb-2">
           <Row>
             <Col>
-              <Location />
-              連江縣莒光鄉福正村56號之1
+              <div className="d-flex align-items-center">
+                <Location className="me-2" />
+                連江縣莒光鄉福正村56號之1
+              </div>
             </Col>
           </Row>
           <Row>
             <Col>
-              <Telephone />
-              08-9672026
+              <div className="d-flex align-items-center">
+                <Telephone className="me-2" />
+                08-9672026
+              </div>
             </Col>
             <Col>
-              <Clock />
-              全天
+              <div className="d-flex align-items-center">
+                <Clock className="me-2" />
+                全天
+              </div>
             </Col>
           </Row>
-        </div> */}
+        </div>
         <CardRef.Text className="fw-normal">{props?.text}</CardRef.Text>
       </CardRef.Body>
     </CardRef>
