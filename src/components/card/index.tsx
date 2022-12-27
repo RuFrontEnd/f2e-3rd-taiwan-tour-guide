6cee053640ce7e0801be09faea68277e88349ad6
RuFrontEnd <k718198@gmail.com>
2022-12-27 21:45:05 +0800

    更換使用 vallina bootstrap

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,48 +1,64 @@
-import CardRef from "react-bootstrap/Card";
+// import CardRef from "react-bootstrap/Card";
 import Types from "./type.d";
 import Styles from "./styles.module.scss";
-import { Container, Row, Col } from "react-bootstrap";
+// import { Container, Row, Col } from "react-bootstrap";
 import { ReactComponent as Location } from "assets/icon/location.svg";
 import { ReactComponent as Telephone } from "assets/icon/telephone.svg";
 import { ReactComponent as Clock } from "assets/icon/clock.svg";
 
 const Card = (props: Types.Props) => {
   return (
-    <CardRef>
-      <div className="fs-1 w-10 opacity-40">123</div>
-      {props?.src && <CardRef.Img variant="top" src={props?.src} />}
-      <CardRef.Body>
-        <CardRef.Title className="fw-bold mb-2 fs-5" title={props?.title}>
-          {props?.title}
-        </CardRef.Title>
-        {/* TODO: 地址 / 聯絡方式 / 開放時段 待製作 */}
-        <div className="fw-normal mb-2">
-          <Row>
-            <Col>
-              <div className="d-flex align-items-center">
-                <Location className="me-2" />
-                連江縣莒光鄉福正村56號之1
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <div className="d-flex align-items-center">
-                <Telephone className="me-2" />
-                08-9672026
-              </div>
-            </Col>
-            <Col>
-              <div className="d-flex align-items-center">
-                <Clock className="me-2" />
-                全天
-              </div>
-            </Col>
-          </Row>
+    <>
+    <div className="fs-1 w-10 opacity-40">123</div>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
         </div>
-        <CardRef.Text className="fw-normal">{props?.text}</CardRef.Text>
-      </CardRef.Body>
-    </CardRef>
+      </div>
+    </>
+    // <CardRef>
+    //   <div className="fs-1 w-10 opacity-40">123</div>
+    //   {props?.src && <CardRef.Img variant="top" src={props?.src} />}
+    //   <CardRef.Body>
+    //     <CardRef.Title className="fw-bold mb-2 fs-5" title={props?.title}>
+    //       {props?.title}
+    //     </CardRef.Title>
+    //     {/* TODO: 地址 / 聯絡方式 / 開放時段 待製作 */}
+    //     <div className="fw-normal mb-2">
+    //       <Row>
+    //         <Col>
+    //           <div className="d-flex align-items-center">
+    //             <Location className="me-2" />
+    //             連江縣莒光鄉福正村56號之1
+    //           </div>
+    //         </Col>
+    //       </Row>
+    //       <Row>
+    //         <Col>
+    //           <div className="d-flex align-items-center">
+    //             <Telephone className="me-2" />
+    //             08-9672026
+    //           </div>
+    //         </Col>
+    //         <Col>
+    //           <div className="d-flex align-items-center">
+    //             <Clock className="me-2" />
+    //             全天
+    //           </div>
+    //         </Col>
+    //       </Row>
+    //     </div>
+    //     <CardRef.Text className="fw-normal">{props?.text}</CardRef.Text>
+    //   </CardRef.Body>
+    // </CardRef>
   );
 };
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap";
+// import { Container, Row, Col } from "react-bootstrap";
 import Card from "components/card";
 import lighthouse from "assets/fake/lighthouse.png";
 
@@ -6,21 +6,29 @@ const keys = [1, 2, 3];
 
 const Home = () => {
   return (
-    <Container>
-      <Row>
-        {keys.map((key) => (
-          <Col sm={12} md={6} xxl={4}>
-            <Card
-              src={lighthouse}
-              title={"東莒島燈塔(東犬燈塔)"}
-              text={
-                "碧海藍天下走過百年歲月的潔白燈塔 東莒島燈塔，又名「東犬燈塔」，位在東莒島的東北方，清朝因鴉片戰..."
-              }
-            />
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <Card
+      src={lighthouse}
+      title={"東莒島燈塔(東犬燈塔)"}
+      text={
+        "碧海藍天下走過百年歲月的潔白燈塔 東莒島燈塔，又名「東犬燈塔」，位在東莒島的東北方，清朝因鴉片戰..."
+      }
+    />
+
+    // <Container>
+    //   <Row>
+    //     {keys.map((key) => (
+    //       <Col sm={12} md={6} xxl={4}>
+    //         <Card
+    //           src={lighthouse}
+    //           title={"東莒島燈塔(東犬燈塔)"}
+    //           text={
+    //             "碧海藍天下走過百年歲月的潔白燈塔 東莒島燈塔，又名「東犬燈塔」，位在東莒島的東北方，清朝因鴉片戰..."
+    //           }
+    //         />
+    //       </Col>
+    //     ))}
+    //   </Row>
+    // </Container>
   );
 };
 
