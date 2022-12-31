36e34c38db2bfc895c902da9f69d9b5996895de8
RuFrontEnd <k718198@gmail.com>
2022-12-31 15:26:35 +0800

    新增 font color utils

--- src/pages/guide/index.tsx
+++ src/pages/guide/index.tsx
@@ -7,9 +7,7 @@ const keys = [1, 2, 3];
 const Guide = () => {
   return (
     <>
-      <h1 className="f-1 text-primary">
-        Font Guide
-      </h1>
+      <h1 className="f-1 text-primary">Font Size</h1>
       <div className="f-1 cursor-pointer">東莒島燈塔(東犬燈塔) h1</div>
       <div className="f-2 cursor-auto">東莒島燈塔(東犬燈塔) h2</div>
       <div className="f-3">東莒島燈塔(東犬燈塔) h3</div>
@@ -22,6 +20,23 @@ const Guide = () => {
       <div className="f-bd2"> 東莒島燈塔(東犬燈塔) Body2</div>
       <div className="f-cp1"> 東莒島燈塔(東犬燈塔) Caption1</div>
       <div className="f-cp2"> 東莒島燈塔(東犬燈塔) Caption2</div>
+      <br />
+      <h1 className="f-3 text-blue-900">Font Color</h1>
+      <div className="f-3 color-tint">Primary Tint</div>
+      <div className="f-3 color-light">Primary Tint</div>
+      <div className="f-3 color-default">Primary Tint</div>
+      <div className="f-3 color-a11y">Primary Tint</div>
+      <div className="f-3 color-black-core">Primary Tint</div>
+      <div className="f-3 color-black-900">Primary Tint</div>
+      <div className="f-3 color-black-800">Primary Tint</div>
+      <div className="f-3 color-black-700">Primary Tint</div>
+      <div className="f-3 color-black-600">Primary Tint</div>
+      <div className="f-3 color-black-500">Primary Tint</div>
+      <div className="f-3 color-black-400">Primary Tint</div>
+      <div className="f-3 color-black-300">Primary Tint</div>
+      <div className="f-3 color-black-200">Primary Tint</div>
+      <div className="f-3 color-black-100">Primary Tint</div>
+      <div className="f-3 color-white bg-dark">Primary Tint</div>
     </>
   );
 };
