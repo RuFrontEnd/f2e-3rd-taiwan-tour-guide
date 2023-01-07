ddc29a4723fc97cc15205327dc687a620cac6aae
Luke_Xie <Luke_Xie@wistron.com>
2023-01-07 14:42:37 +0800

    調整 header 間距

--- src/pages/home/header/index.tsx
+++ src/pages/home/header/index.tsx
@@ -15,17 +15,17 @@ const Home = () => {
 
   return (
     <header className="bg-light">
-      <div className="container-lg">
+      <div className="container-fluid container-lg">
         <div className="row">
-          <div className="col-12 col-sm-6 g-0">
-            <div className="d-flex justify-content-center align-items-center pt-6 px-8 pb-2 pt-sm-0 px-sm-0 pb-sm-0">
-              <img src={travelingByTrain} className="w-100 mh-540px object-fit-contain"></img>
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-6 g-0">
-            <div className="h-100 d-flex justify-content-center align-items-center pb-4 pb-sm-0">
-              <div className="d-flex flex-sm-column">
+          <div className="col-12 g-0">
+            <div className="d-flex flex-column flex-sm-row justify-content-center align-items-center">
+              <div className="me-md-16 me-lg-26 mt-6 mb-2">
+                <img
+                  src={travelingByTrain}
+                  className="mh-280px mh-sm-350px mh-lg-540px object-fit-contain"
+                ></img>
+              </div>
+              <div className="d-flex flex-sm-column mb-6 mb-sm-0">
                 <h1 className="f-3 f-md-1 text-a11y mb-0 me-2 me-sm-0 pe-sm-22 pe-md-30">
                   台灣景點
                 </h1>
