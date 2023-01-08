a5d260c4e15d7c9e0ee1e1b5cf0ca86e1999663a
RuFrontEnd <k718198@gmail.com>
2023-01-08 18:58:15 +0800

    切版熱門關鍵字下拉選單

--- src/components/searchInput/index.tsx
+++ src/components/searchInput/index.tsx
@@ -1,10 +1,16 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect, MouseEvent } from "react";
 import Types from "./type";
 import { ReactComponent as Magnifier } from "assets/icon/magnifier.svg";
 
-const SearchInput = (props: Types.Props) => {
-  const $test: any = useRef(null);
+const hotKeywords = [
+  "台南文化",
+  "嘉義觀光工廠",
+  "台東自然風景",
+  "屏東國家風景區",
+  "新竹遊憩",
+];
 
+const SearchInput = (props: Types.Props) => {
   const [dropdownVisible, setDropdownVisible] = useState(false);
 
   const closeDropdown = () => {
@@ -15,22 +21,25 @@ const SearchInput = (props: Types.Props) => {
     setDropdownVisible(true);
   };
 
-  const onBlurTest = () => {
-    closeDropdown();
+  const onClickContainer = (e: MouseEvent<HTMLElement>) => {
+    e.stopPropagation();
+    openDropdown();
   };
 
-  const onClickTest = () => {
-    openDropdown();
-    if (!$test) {
-      $test.focus();
-    }
-  };
+  useEffect(() => {
+    document.body.addEventListener("click", closeDropdown);
+
+    return () => {
+      document.body.removeEventListener("click", closeDropdown);
+    };
+  }, []);
 
   return (
     <div
       className={`${
         props.className ? props.className : ""
       } position-relative d-inline-block`}
+      onClick={onClickContainer}
     >
       <Magnifier
         width={18}
@@ -38,23 +47,29 @@ const SearchInput = (props: Types.Props) => {
         className={"position-absolute top-50p start-4_5 translate-middle-y"}
       />
 
-      <div ref={$test} onClick={onClickTest} onBlur={onBlurTest}>
-        <input
-          className="border border-1 bd-color-black-core py-3_5 ps-10_5 pe-4_5 rounded-pill"
-          placeholder={props?.placeholder}
-          value={props.value}
-        />
-        {dropdownVisible && (
-          <div
-            className="position-absolute w-100 border border-1 bd-color-black-core z-index-10 bg-white"
-            onFocus={() => {
-              console.log("AAA");
-            }}
-          >
-            123
-          </div>
-        )}
-      </div>
+      <input
+        className="border border-1 bd-color-black-core py-3_5 ps-10_5 pe-4_5 rounded-pill"
+        placeholder={props?.placeholder}
+        value={props.value}
+      />
+      {dropdownVisible && (
+        <div
+          className="position-absolute w-100 border border-1 bd-color-black-core z-index-10 bg-white px-4 py-3"
+          role={"search-input-dropdown"}
+        >
+          <caption className="f-cp1 d-block p-0 ">熱門關鍵字</caption>
+          <ul>
+            {hotKeywords.map((hotKeyword, hotKeywordIndex) => (
+              <>
+                <li className="pt-3 f-bd1">{hotKeyword}</li>
+                {hotKeywordIndex !== hotKeywords.length - 1 && (
+                  <div className="pt-3 border-bottom"></div>
+                )}
+              </>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
