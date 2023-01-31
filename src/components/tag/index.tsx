df0fd96e00694e4452d7adf9cf00d5236b528e16
RuFrontEnd <k718198@gmail.com>
2023-01-31 20:55:14 +0800

    新增 Tag 元件

---
+++ src/components/tag/index.tsx
@@ -0,0 +1,18 @@
+import * as Types from "types/";
+import { ReactComponent as X } from "assets/icon/x.svg";
+
+const Tag = (props: Types.Components.Tag.Props) => {
+  return (
+    <button
+      onClick={props.onClick}
+      className={`${
+        props.className ? props.className : ""
+      } text-a11y border-color-a11y border-1 border-radius-5 px-2 py-1 d-flex align-items-center`}
+    >
+      {props.text}
+      <X className="ms-1" />
+    </button>
+  );
+};
+
+export default Tag;
